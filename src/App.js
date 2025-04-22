6603f64e80da22a8f2d216144ad3f012e5478edc
Abdelrahman <bm1208347@gmail.com>
2025-04-22 22:27:30 +0200

    home page was created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+// My components
 import Header from './components/header/Header';
-
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <main className='main'>
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home_social">
+            <a href="https://wa.link/58p9ck" className="home_social-icon" target='_blank'>
+                <i className='uil uil-whatsapp'></i>
+            </a>
+            <a href="https://www.linkedin.com/in/abdelrahman-salem-182014293?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className="home_social-icon" target='_blank'>
+                <i className='uil uil-linkedin-alt'></i>
+            </a>
+            <a href="https://github.com/AboDa7m88" className="home_social-icon" target='_blank'>
+                <i className='uil uil-github-alt'></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
